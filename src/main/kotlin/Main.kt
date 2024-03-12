6e61817c3028854e772c4d089e76fc0d5a30181a
Isabel Estevez <isabell.estevezz@gmail.com>
2024-03-12 14:23:34 +0100

    Refact_1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,7 +27,7 @@ fun main(args: Array<String>) {
         f++
     }
 
-    sumar2(m,sumaColum)
+    sumaColumnas(m,sumaColum)
     println("La suma de las filas es")
     println(Arrays.toString(sumaFilas))
     println("La suma de las columnas es")
@@ -37,7 +37,7 @@ fun main(args: Array<String>) {
 }
 
 
-fun sumar2 (m:Array<Array<Int>>, sumaColumnas:Array<Int>){
+fun sumaColumnas (m:Array<Array<Int>>, sumaColumnas:Array<Int>){
     var f: Int = 0
     var c: Int = 0
 
